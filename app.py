a7cf538a5c4053140dc966af4fdeb1c6f949c536
klos71 <jonas97_hellman@hotmail.com>
2019-01-21 12:06:36 +0200

    removed json config, merged to database, added simple inventory system with add option

--- app.py
+++ app.py
@@ -1,11 +1,63 @@
-from flask import Flask, render_template, request
-import json
+from flask import Flask, render_template, request, g, redirect, url_for
+from flask_login import LoginManager, UserMixin
+import json, sqlite3, flask_login
+
+login_manager = LoginManager()
 
 app = Flask(__name__, static_url_path='')
 app.secret_key = 'super secret string'
 
-with open('config.json') as f:
-    data = json.load(f)
+login_manager.init_app(app)
+
+DATABASE = "database.db"
+
+class User(UserMixin):
+    pass
+
+@login_manager.user_loader
+def user_loader(email):
+
+    user = User()
+    user.id = email
+    return user
+
+
+@login_manager.request_loader
+def request_loader(request):
+    email = request.form.get('email')
+    temp = []
+    for user in query_db('select * from admin'):
+        temp.append(user)
+    if email not in temp:
+        return
+
+    user = User()
+    user.id = email
+
+    temp = query_db('SELECT password FROM admin WHERE username = ?', [email], one=True)
+    user.is_authenticated = request.form['password'] == str(temp).split("'")[1]
+
+    return user
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+
+def query_db(query, args=(), one=False):
+    cur = get_db().execute(query, args)
+    rv = cur.fetchall()
+    cur.close()
+    return (rv[0] if rv else None) if one else rv
+
+
+def database():
+    global conn, cursor
+    conn = sqlite3.connect(DATABASE)
+    cursor = conn.cursor()
+
 
 @app.route('/')
 def hello_world():
@@ -13,10 +65,61 @@ def hello_world():
 
 
 @app.route('/inventory')
+@flask_login.login_required
 def inventory():
-    return render_template("inv.html")
+    inv = []
+    database()
+    cursor.execute("SELECT * FROM `product`")
+    fetch = cursor.fetchall()
+    for data in fetch:
+        temp = dict(name=data[1],qty=data[2],price=data[3])
+        inv.append(temp)
 
+    return render_template("inv.html",inv=inv)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template("login.html")
+
+    email = request.form['email']
+    temp = query_db('SELECT password FROM admin WHERE username = ?', [email], one=True)
+    print str(temp).split("'")[1]
+
+    if str(temp).split("'")[1] == request.form['password']:
+        user = User()
+        user.id = email
+        flask_login.login_user(user)
+        return redirect(url_for('inventory'))
+    msg = "Wrong username/password"
+    return render_template("login.html", msg=msg)
+
+@app.route('/logout')
+@flask_login.login_required
+def logout():
+    flask_login.logout_user()
+    return render_template("index.html")
+
+@app.route("/add", methods=['GET', 'POST'])
+@flask_login.login_required
+def add_prod():
+    if request.method == 'GET':
+        return render_template("addProd.html")
+
+    database()
+    print request.form["name"]
+    cursor.execute("INSERT INTO `product` (product_name, product_qty, product_price) VALUES(?, ?, ?)",(str(request.form['name']), int(request.form['QTY']), int(request.form['price'])))
+    conn.commit()
+    cursor.close()
+    conn.close()
+    return render_template("addProd.html", msg="Succees")
+
+@login_manager.unauthorized_handler
+def unauthorized_handler():
+    return render_template("login.html")
 
 @app.context_processor
 def inject_Cname():
-    return dict(Cname=data["config"][0]["CompanyName"])
+    name = query_db("SELECT company_name FROM config")
+    return dict(Cname=str(name).split("'")[1])
+
--- installation.py
+++ installation.py
@@ -1,15 +1,28 @@
-import json
+import sqlite3
 
-data = {}
-#print("Enter Company Name")
-name = raw_input("Enter Company Name")
-print(name)
+DATABASE = "database.db"
 
-data['config'] = []
 
-data['config'].append({
-    'CompanyName': name
-})
+def database():
+    global conn, cursor
+    conn = sqlite3.connect(DATABASE)
+    cursor = conn.cursor()
+    cursor.execute("CREATE TABLE IF NOT EXISTS `admin` (admin_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, username TEXT, password TEXT)")
+    cursor.execute("CREATE TABLE IF NOT EXISTS `config` (config_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, company_name TEXT)")
+    cursor.execute("CREATE TABLE IF NOT EXISTS `product` (product_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, product_name TEXT, product_qty TEXT, product_price TEXT)")
+    cursor.execute("SELECT * FROM `admin` WHERE `username` = 'admin' AND `password` = 'admin'")
+    if cursor.fetchone() is None:
+        cursor.execute("INSERT INTO `admin` (username, password) VALUES('admin', 'admin')")
+        conn.commit()
+    cursor.execute("SELECT * from config WHERE config_id = 1")
+    if cursor.fetchone() is None:
+        pass
+    else:
+        cursor.execute('INSERT INTO config (company_name) VALUES ("test")')
 
-with open('config.json', 'w') as outfile:
-    json.dump(data, outfile)
+
+database()
+print("Insert Company name")
+name = raw_input()
+cursor.execute("UPDATE config SET company_name = ? WHERE config_id = 1", (name,))
+conn.commit()
